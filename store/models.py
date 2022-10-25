1f4485ec8b55abb8e1e62da36ce74ad94c0461bd
Raza <razau314@gmail.com>
2022-10-25 09:59:42 +0500

    code for getting and updating a cart item

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from wsgiref.validate import validator
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -103,7 +104,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
 
     class Meta:
         unique_together = [['product', 'cart']]
--- store/views.py
+++ store/views.py
@@ -8,7 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from store.filters import ProductFilter
 from store.pagination import DefaultPagination
 
-from store.serializers import CartItemSerializer, CartSerializer, ProductSerialzier, CollectionSerializer, ReviewSerializer
+from store.serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, ProductSerialzier, CollectionSerializer, ReviewSerializer, UpdateCartItemSerializer
 from .models import Cart, CartItem, Collection, OrderItem, Product, Review
 
 
@@ -64,7 +64,16 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
 
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
